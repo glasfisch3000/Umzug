70270cf61d55edb4290950692194a29bba336328
glasfisch3000 <jakob@danckwerts.net>
2024-11-20 12:18:32 +0100

    initial commit

---
+++ Umzug/UmzugApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmzugApp.swift
+//  Umzug
+//
+//  Created by Jakob Danckwerts on 20.11.24.
+//
+
+import SwiftUI
+
+@main
+struct UmzugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
