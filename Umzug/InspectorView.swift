8b013e4015e470eb07dc877137a33aebe8398fd3
glasfisch3000 <jakob@danckwerts.net>
2024-11-28 17:12:55 +0100

    add simple inspector view and boxes view

--- Umzug/InspectorView.swift
+++ Umzug/InspectorView.swift
@@ -8,22 +8,35 @@
 import SwiftUI
 
 struct InspectorView: View {
+    enum NavigationContent: Hashable {
+        case boxes
+    }
+    
     @Environment(\.verticalSizeClass) private var verticalSizeClass
     
     var api: UmzugAPI
     
+    @State private var presentedPath: [NavigationContent] = []
+    
     var body: some View {
-        GeometryReader { geo in
-            if geo.size.width > geo.size.height {
-                HStack(alignment: .center, spacing: 20) {
-                    navigationLinks
+        NavigationStack(path: $presentedPath) {
+            GeometryReader { geo in
+                if geo.size.width > geo.size.height {
+                    HStack(alignment: .center, spacing: 20) {
+                        navigationLinks
+                    }
+                    .padding()
+                } else {
+                    VStack(alignment: .center, spacing: 20) {
+                        navigationLinks
+                    }
+                    .padding()
                 }
-                .padding()
-            } else {
-                VStack(alignment: .center, spacing: 20) {
-                    navigationLinks
+            }
+            .navigationDestination(for: NavigationContent.self) { path in
+                switch path {
+                case .boxes: BoxesView(boxes: UmzugFetched(api: api, request: .boxes))
                 }
-                .padding()
             }
         }
     }
@@ -31,25 +44,28 @@ struct InspectorView: View {
     @ViewBuilder
     private var navigationLinks: some View {
         Group {
-            ZStack {
-                RoundedRectangle(cornerRadius: 40)
-                    .foregroundStyle(.red)
-                    .opacity(0.3)
-                
-                VStack(alignment: .center, spacing: 15) {
-                    Image(systemName: "shippingbox")
-                        .font(.system(size: 60))
+            NavigationLink(value: NavigationContent.boxes) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 40)
+                        .foregroundStyle(.red)
+                        .opacity(0.3)
                     
-                    HStack(alignment: .center) {
-                        Text("Boxes")
-                            .font(.title)
-                            .fontWeight(.semibold)
+                    VStack(alignment: .center, spacing: 15) {
+                        Image(systemName: "shippingbox")
+                            .font(.system(size: 60))
                         
-                        Image(systemName: "chevron.forward")
-                            .font(.title)
+                        HStack(alignment: .center) {
+                            Text("Boxes")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            
+                            Image(systemName: "chevron.forward")
+                                .font(.title)
+                        }
                     }
                 }
             }
+            .foregroundStyle(.primary)
             
             ZStack {
                 RoundedRectangle(cornerRadius: 40)
