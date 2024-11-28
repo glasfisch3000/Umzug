550f57e80d4fd0928db201e674c4e28884b3deb8
glasfisch3000 <jakob@danckwerts.net>
2024-11-28 16:59:45 +0100

    update api fetching

--- Umzug/UmzugAPI/APIError.swift
+++ Umzug/UmzugAPI/APIError.swift
@@ -5,12 +5,18 @@
 //  Created by Jakob Danckwerts on 24.11.24.
 //
 
+import NIOHTTP1
+
 extension UmzugAPI {
-    enum APIError: Sendable, Error {
+    enum APIError: Sendable, Error, Decodable {
         case invalidURL
-        case invalidHeader
         case invalidAuthentication
         case clientShutdown
+        case invalidStatus(HTTPResponseStatus)
         case other
+        
+        enum CodingKeys: CodingKey {
+            case invalidAuthentication
+        }
     }
 }
--- Umzug/UmzugAPI/UmzugAPI.swift
+++ Umzug/UmzugAPI/UmzugAPI.swift
@@ -11,6 +11,8 @@ import NIOCore
 import AsyncHTTPClient
 
 struct UmzugAPI: DynamicProperty, APIProtocol {
+    typealias Response = ByteBuffer?
+    
     var client: HTTPClient
     var server: APIServer
     var authentication: Authentication
@@ -31,23 +33,36 @@ struct UmzugAPI: DynamicProperty, APIProtocol {
             return "\(key)=\(value)"
         }.joined(separator: "&")
         
-        let url = "\(self.server.scheme)://\(self.server.host):\(self.server.port)/\(path)?\(query)"
-        
-        // test
-        let error = HTTPClientError.invalidURL
-        let mirror = Mirror(reflecting: error)
-        print(mirror.children)
+        let url = "\(self.server.scheme)://\(self.server.host):\(self.server.port)/api/\(path)?\(query)"
         
         do {
-            let result = try await self.client.get(url: url, deadline: .now() + .seconds(1)).get()
-            return Response(statusCode: result.status, body: result.body)
+            let result = try await self.client.get(url: url, deadline: .now() + .seconds(10)).get()
+            
+            guard result.status == .ok else {
+                throw APIError.invalidStatus(result.status)
+            }
+            
+            struct APIErrorResponse: Decodable {
+                var error: APIError
+            }
+            
+            if let body = result.body, let error = try? JSONDecoder().decode(APIErrorResponse.self, from: body) {
+                throw error.error
+            }
+            
+            return result.body
+        } catch let error as APIError {
+            print(error)
+            throw error
         } catch let error as HTTPClientError {
+            print(error)
             switch error {
             case .invalidURL, .emptyScheme, .emptyHost: throw APIError.invalidURL
             case .alreadyShutdown: throw APIError.clientShutdown
             default: throw APIError.other
             }
         } catch {
+            print(error)
             throw APIError.other
         }
     }
